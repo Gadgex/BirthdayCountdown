c6dc868a6ee45cda05a45e6e55626b5bbfe5b742
Gadgex <jaxfair02@gmail.com>
2018-07-31 12:08:18 -0600

    fixed timer state bug and got timer object into clock.js JSX

--- src/components/app.js
+++ src/components/app.js
@@ -12,9 +12,9 @@ export default class App extends Component {
   constructor(props) {
     super(props)
 
-    var timer = 0;
+    this.timer = 0;
 
-    this.state= {
+    this.state = {
       active: false,
       startDate: moment(),
       timeRemaining: {
@@ -28,68 +28,68 @@ export default class App extends Component {
     this.handleGenerate = this.handleGenerate.bind(this)
   }
 
-  handleChange = function(date) {
+  handleChange = function (date) {
     console.log('APP JS Handle change', date._d);
     this.setState({
-        startDate: date
+      startDate: date
     });
   }.bind(this)
 
-  handleGenerate = function() {
+  handleGenerate = function () {
     this.setState({ active: true })
     // Set the date we're counting down to
 
     var countDownDate = this.state.startDate.toDate().getTime();
-  
+
     // Update the count down every 1 second 
-    timer = setInterval(function() {
+    this.timer = setInterval(function () {
 
-    // Get todays date and time
-    var now = new Date().getTime();
-    
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+      // Get todays date and time
+      var now = new Date().getTime();
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / (1000));
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
 
-    // Output the result in an element with id="demo"
-   const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / (1000));
+
+      // Output the result in an element with id="demo"
+      const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
       const timeRemaining = {
         days,
         hours,
         minutes,
         seconds
       }
-      this.setState({ active: true })
+      this.setState({ timeRemaining })
 
-   console.log(this.state.timeRemaining);
+      console.log(this.state.timeRemaining);
 
-   // If the count down is over, write some text
-   if (distance < 0) {
-      clearInterval(x);
-      // document.getElementById("demo").innherHTML = "EXPIRED";
-    }
-}, 1000);
+      // If the count down is over, write some text
+      if (distance < 0) {
+        clearInterval(this.timer);
+        // document.getElementById("demo").innherHTML = "EXPIRED";
+      }
+    }.bind(this), 1000);
   }.bind(this)
 
-  renderItems = function() {
-    if(this.state.active) {
+  renderItems = function () {
+    if (this.state.active) {
       return [
-        <Clock/>,
-        ChangeDate('Change Date', () => this.setState({active: false})),
+        <Clock timeRemaining={this.state.timeRemaining}/>,
+        ChangeDate('Change Date', () => this.setState({ active: false })),
         LargeText('04/03'),
         <label className="grid__remaining"> Remaining until your birthday </label>
       ]
     } else {
-     return  [
-          <Picker startDate={this.state.startDate} callback={(date) => this.handlechange(date)}/>,
+      return [
+        <Picker startDate={this.state.startDate} callback={(date) => this.handleChange(date)} />,
         Button('Generate Countdown', () => this.handleGenerate())
-        ]
-      }
+      ]
+    }
   }.bind(this)
 
   render() {
@@ -101,9 +101,9 @@ export default class App extends Component {
         <div className="grid__skew-light-one"></div>
         <div className="grid__skew-light-two"></div>
         <div className="grid__skew-light-three-box"></div>
-      
-        
-        {this.renderItems() }
+
+
+        {this.renderItems()}
       </div>
     );
   }
