c499bfa7650b665067f14b137ca2e55053c81aa6
Gadgex <jaxfair02@gmail.com>
2018-07-31 11:19:59 -0600

    refactored app.js and picker.js to have start date

--- src/components/app.js
+++ src/components/app.js
@@ -5,15 +5,58 @@ import Clock from './clock';
 import ChangeDate from './changeDate';
 import LargeText from './largeText';
 
+import moment from 'moment';
+
 export default class App extends Component {
 
   constructor(props) {
     super(props)
 
     this.state= {
-      active: false
+      active: false,
+      startDate: moment()
     }
   }
+
+  handleChange = function(date) {
+    console.log('APP JS Handle change', date._d);
+    this.setState({
+        startDate: date
+    });
+  }.bind(this)
+
+  handleGenerate = function() {
+    this.setState({ active: true })
+    // Set the date we're counting down to
+
+    var countDownDate = this.state.startDate.toDate().getTime();
+  
+    // Update the count down every 1 second 
+    var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / (1000));
+
+    // Output the result in an element with id="demo"
+   const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+   console.log(time)
+
+   // If the count down is over, write some text
+   if (distance < 0) {
+      clearInterval(x);
+      // document.getElementById("demo").innherHTML = "EXPIRED";
+    }
+}, 1000);
+  }.bind(this)
 
   renderItems = function() {
     if(this.state.active) {
@@ -25,8 +68,8 @@ export default class App extends Component {
       ]
     } else {
      return  [
-          <Picker/>,
-        Button('Generate Countdown', () => this.setState ({active: true}))
+          <Picker callback={(date) => this.handlechange(date)}/>,
+        Button('Generate Countdown', () => this.handleGenerate())
         ]
       }
   }.bind(this)
@@ -34,7 +77,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="grid">
-        <h1 className="grid__title">Birthday Countdown </h1>
+        <h1 className="grid__title">Birthday Countdown</h1>
         <div className="grid__skew-dark-two"></div>
 
         <div className="grid__skew-light-one"></div>
